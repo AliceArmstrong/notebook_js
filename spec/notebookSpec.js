515eb2ce1ad412eab68aeb509a13a4227280074f
Alice Armstrong <tioma001@hotmail.com>
2017-03-31 17:59:10 +0100

    Styling added to notebook

--- spec/notebookSpec.js
+++ spec/notebookSpec.js
@@ -1,5 +1,3 @@
-// TODO create a note and see that it's created
-
 // before each would be ideal here
 var notebook = new Notebook();
 
@@ -10,8 +8,5 @@ new Wish("Notebook creates a note with some text", notebook.createNote("I am the
 new Wish("Notebook can give us a list of notes", notebook.notes[0]).isType("Note");
 new Wish("Notebook can write out a list of note abbreviations", notebook.printNoteAbbr()[0].innerHTML).isEqualTo("I am the eggman, I a...");
 new Wish("Notebook's abbreviation html elements have ids", notebook.printNoteAbbr()[0].id).isEqualTo("note5");
-//expect noteArr to contain note
 
-// TODO can list the notes
-
-// can display an individual note
+// TODO WISH ACCEPT ANON FUNCTION
