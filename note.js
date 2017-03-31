71207965b16e4a06216472ade2eaf345fc018bea
Alex Imm <alexanderimm@hotmail.co.uk>
2017-03-31 12:28:37 +0100

    print note time and refactored the formatting of the time

--- note.js
+++ note.js
@@ -4,5 +4,9 @@ function Note(noteBody = ""){
 };
 
 Note.prototype.abbr = function(char) {
-  return this.body.substring(0, char) + "...";
+  return this.getFormattedDate() + this.body.substring(0, char) + "...";
+};
+
+Note.prototype.getFormattedDate = function () {
+  return (this.createdAt.getHours() + ":" + this.createdAt.getMinutes() + " ")
 };
