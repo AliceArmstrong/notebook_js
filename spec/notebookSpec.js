1120f02614e7c5b6a86de0d423ea52acf5df0278
nryn <narayanohanlon@gmail.com>
2017-04-01 12:22:41 +0100

    fixing failing tests since the spike

--- spec/notebookSpec.js
+++ spec/notebookSpec.js
@@ -8,9 +8,8 @@ new Wish("Notebook has an array", notebook.notes).isType("Array")
 new Wish("Notebook can create a note", notebook.createNote()).isType("Note")
 new Wish("Notebook creates a note with some text", notebook.createNote("I am the eggman, I am the walrus").body).isEqualTo("I am the eggman, I am the walrus")
 new Wish("Notebook can give us a list of notes", notebook.notes[0]).isType("Note");
-new Wish("Notebook can write out a list of note abbreviations", notebook.printNoteAbbr()[0].innerHTML).isEqualTo("I am the eggman, I a...");
-new Wish("Notebook's abbreviation html elements have ids", notebook.printNoteAbbr()[0].id).isEqualTo("note5");
-new Wish("Notebook can write out a list of note abbreviations", notebook.printNoteAbbr()[0].innerHTML).isEqualTo(time.getHours() + ":" + time.getMinutes() + " I am the eggman, I a...");
+new Wish("Notebook can write out a list of note abbreviations", notebook.printNoteAbbr()[0].innerHTML).isEqualTo("I am the eggman, I am the walr . . .");
+new Wish("Notebook's abbreviation html elements have ids", notebook.printNoteAbbr()[0].id).isEqualTo("note7");
 unfreezeTime();
 
 // TODO WISH ACCEPT ANON FUNCTION
